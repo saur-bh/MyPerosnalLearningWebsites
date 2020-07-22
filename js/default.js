61457c6d40c23701a65e8d7b7ca6f84ac13e5c6b
Saurabh Verma <saurabh-verma@outlook.com>
2020-07-22 12:48:49 +0530

    Adding few changes

--- js/default.js
+++ js/default.js
@@ -11,6 +11,30 @@ function setDate(){
     return new Date();
 }
 
+//This function will lock priority section 
+function lockpriporitySection(){
+
+    var locktextArea = document.getElementById('alltask0d').setAttribute("readonly","true");
+
+    var dt = document.getElementById('lockDate-id').value;
+     
+
+    var currentDate = new Date();
+    currentDate = formatDate(currentDate);
+
+    // Check the currentDate and 
+
+}
+
+
+function formatDate( date){
+
+    var day = date.getDate();
+     var year = date.getFullYear();
+     var month = date.getMonth()+1;
+
+     return (year+ "-"+month+"-"+day);
+}
 
 
 
